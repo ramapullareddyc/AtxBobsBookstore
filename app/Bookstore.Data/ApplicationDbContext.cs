0e52617b74cae14bbf5b5efc882bfdaf5ca9036a
NoReply Amazon <no-reply@amazon.com>
2025-11-30 17:12:47 +0000

    DB transformation: Upload transformed code

--- app/Bookstore.Data/ApplicationDbContext.cs
+++ app/Bookstore.Data/ApplicationDbContext.cs
@@ -49,10 +49,11 @@ namespace Bookstore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Table and column mappings
+            // Schema and table mappings for all entities
             modelBuilder.Entity<Address>(entity =>
             {
                 entity.ToTable("Address", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.AddressLine1).HasColumnName("AddressLine1");
                 entity.Property(e => e.AddressLine2).HasColumnName("AddressLine2");
                 entity.Property(e => e.City).HasColumnName("City");
@@ -60,8 +61,7 @@ namespace Bookstore.Data
                 entity.Property(e => e.Country).HasColumnName("Country");
                 entity.Property(e => e.ZipCode).HasColumnName("ZipCode");
                 entity.Property(e => e.CustomerId).HasColumnName("CustomerId");
-                entity.Property(e => e.IsActive).HasColumnName("IsActive");
-                entity.Property(e => e.Id).HasColumnName("Id");
+                entity.Property(e => e.IsActive).HasColumnName("IsActive").HasConversion<int>();
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
@@ -70,6 +70,7 @@ namespace Bookstore.Data
             modelBuilder.Entity<Book>(entity =>
             {
                 entity.ToTable("Book", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.Name).HasColumnName("Name");
                 entity.Property(e => e.Author).HasColumnName("Author");
                 entity.Property(e => e.Year).HasColumnName("Year");
@@ -82,15 +83,16 @@ namespace Bookstore.Data
                 entity.Property(e => e.Summary).HasColumnName("Summary");
                 entity.Property(e => e.Price).HasColumnName("Price");
                 entity.Property(e => e.Quantity).HasColumnName("Quantity");
-                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
+                // Note: IsInStock and IsLowInStock are computed properties (no database column)
             });
 
             modelBuilder.Entity<Customer>(entity =>
             {
                 entity.ToTable("Customer", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.Sub).HasColumnName("Sub");
                 entity.Property(e => e.Username).HasColumnName("Username");
                 entity.Property(e => e.FirstName).HasColumnName("FirstName");
@@ -98,7 +100,6 @@ namespace Bookstore.Data
                 entity.Property(e => e.Email).HasColumnName("Email");
                 entity.Property(e => e.DateOfBirth).HasColumnName("DateOfBirth");
                 entity.Property(e => e.Phone).HasColumnName("Phone");
-                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
@@ -107,21 +108,22 @@ namespace Bookstore.Data
             modelBuilder.Entity<Order>(entity =>
             {
                 entity.ToTable("Order", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CustomerId).HasColumnName("CustomerId");
                 entity.Property(e => e.AddressId).HasColumnName("AddressId");
                 entity.Property(e => e.DeliveryDate).HasColumnName("DeliveryDate");
                 entity.Property(e => e.OrderStatus).HasColumnName("OrderStatus");
-                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
+                // Note: Tax, SubTotal, Total are computed properties (no database column)
             });
 
             modelBuilder.Entity<ShoppingCart>(entity =>
             {
                 entity.ToTable("ShoppingCart", "dbo");
-                entity.Property(e => e.CorrelationId).HasColumnName("CorrelationId");
                 entity.Property(e => e.Id).HasColumnName("Id");
+                entity.Property(e => e.CorrelationId).HasColumnName("CorrelationId");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
@@ -130,11 +132,11 @@ namespace Bookstore.Data
             modelBuilder.Entity<ShoppingCartItem>(entity =>
             {
                 entity.ToTable("ShoppingCartItem", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.ShoppingCartId).HasColumnName("ShoppingCartId");
                 entity.Property(e => e.BookId).HasColumnName("BookId");
                 entity.Property(e => e.Quantity).HasColumnName("Quantity");
-                entity.Property(e => e.WantToBuy).HasColumnName("WantToBuy");
-                entity.Property(e => e.Id).HasColumnName("Id");
+                entity.Property(e => e.WantToBuy).HasColumnName("WantToBuy").HasConversion<int>();
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
@@ -143,10 +145,10 @@ namespace Bookstore.Data
             modelBuilder.Entity<OrderItem>(entity =>
             {
                 entity.ToTable("OrderItem", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.OrderId).HasColumnName("OrderId");
                 entity.Property(e => e.BookId).HasColumnName("BookId");
                 entity.Property(e => e.Quantity).HasColumnName("Quantity");
-                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
@@ -155,6 +157,7 @@ namespace Bookstore.Data
             modelBuilder.Entity<Offer>(entity =>
             {
                 entity.ToTable("Offer", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.Author).HasColumnName("Author");
                 entity.Property(e => e.ISBN).HasColumnName("ISBN");
                 entity.Property(e => e.BookName).HasColumnName("BookName");
@@ -168,7 +171,6 @@ namespace Bookstore.Data
                 entity.Property(e => e.Comment).HasColumnName("Comment");
                 entity.Property(e => e.CustomerId).HasColumnName("CustomerId");
                 entity.Property(e => e.BookPrice).HasColumnName("BookPrice");
-                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
@@ -177,6 +179,7 @@ namespace Bookstore.Data
             modelBuilder.Entity<Author>(entity =>
             {
                 entity.ToTable("Author", "dbo");
+                entity.HasKey(e => e.BusinessEntityID);
                 entity.Property(e => e.BusinessEntityID).HasColumnName("BusinessEntityID");
                 entity.Property(e => e.NationalIDNumber).HasColumnName("NationalIDNumber");
                 entity.Property(e => e.LoginID).HasColumnName("LoginID");
@@ -192,6 +195,7 @@ namespace Bookstore.Data
             modelBuilder.Entity<Product>(entity =>
             {
                 entity.ToTable("Product", "dbo");
+                entity.HasKey(e => e.ProductID);
                 entity.Property(e => e.ProductID).HasColumnName("ProductID");
                 entity.Property(e => e.Name).HasColumnName("Name");
                 entity.Property(e => e.ProductNumber).HasColumnName("ProductNumber");
@@ -201,14 +205,15 @@ namespace Bookstore.Data
             modelBuilder.Entity<ReferenceDataItem>(entity =>
             {
                 entity.ToTable("ReferenceData", "dbo");
+                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.DataType).HasColumnName("DataType");
                 entity.Property(e => e.Text).HasColumnName("Text");
-                entity.Property(e => e.Id).HasColumnName("Id");
                 entity.Property(e => e.CreatedBy).HasColumnName("CreatedBy");
                 entity.Property(e => e.CreatedOn).HasColumnName("CreatedOn");
                 entity.Property(e => e.UpdatedOn).HasColumnName("UpdatedOn");
             });
 
+            // Existing relationships and indexes
             modelBuilder.Entity<Customer>().HasIndex(x => x.Sub).IsUnique();
 
             modelBuilder.Entity<Book>().HasOne(x => x.Publisher).WithMany().HasForeignKey(x => x.PublisherId).OnDelete(DeleteBehavior.Restrict);
@@ -222,10 +227,6 @@ namespace Bookstore.Data
             modelBuilder.Entity<Offer>().HasOne(x => x.Condition).WithMany().HasForeignKey(x => x.ConditionId).OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Order>().HasOne(x => x.Customer).WithMany().OnDelete(DeleteBehavior.Restrict);
-
-            // PostgreSQL boolean to integer conversions for EF Core
-            modelBuilder.Entity<Address>().Property(e => e.IsActive).HasConversion<int>();
-            modelBuilder.Entity<ShoppingCartItem>().Property(e => e.WantToBuy).HasConversion<int>();
 
             PopulateDatabase(modelBuilder);
 
